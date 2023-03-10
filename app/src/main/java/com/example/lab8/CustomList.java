3762919af6ab160abf78bb20d78bc42f4161e450
Frances <francesiigwe@gmail.com>
2023-03-10 07:36:56 -0700

    implemented hasCity method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -75,8 +75,8 @@ public class CustomList extends ArrayAdapter<City> {
      *
      * */
 
-   // public boolean hasCity(City city){
-     //   return cities.contains(city);}
+    public boolean hasCity(City city){
+        return cities.contains(city);}
 
 
 
